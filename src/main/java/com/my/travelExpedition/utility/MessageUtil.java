5bcb31470afe3400f3edd3e1412ce8aabfcea778
Kwanhyung <kwan190119@gmail.com>
2021-09-27 09:51:22 +0900

    Revert "utility 경로 재 변경"
    This reverts commit 785bc4ddfe3913753225e146d50fbd23420ac035.

--- src/main/java/com/my/travelExpedition/utility/MessageUtil.java
+++ src/main/java/com/my/travelExpedition/utility/MessageUtil.java
@@ -1,4 +1,4 @@
-package utility;
+package com.my.travelExpedition.utility;
 
 import java.util.Locale;
 
--- src/main/java/com/my/travelExpedition/utility/Paging.java
+++ src/main/java/com/my/travelExpedition/utility/Paging.java
@@ -1,4 +1,4 @@
-package utility;
+package com.my.travelExpedition.utility;
 
 public class Paging {
 
