fe192a91121805953e25b2dd522aa2e361e64a15
남현선 <ttlsun89@gmail.com>
2021-09-30 14:13:30 +0900

    1. 이미지 테이블에 update, delect 추가. 2. 구현 다들 하시면 추후 콘솔로 찍은것들은 주석처리 요망.

--- src/main/java/user/postimg/model/PostimgDao.java
+++ src/main/java/user/postimg/model/PostimgDao.java
@@ -1,5 +1,6 @@
 package user.postimg.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,9 +21,35 @@ public class PostimgDao {
 	}
 	
 	public int insertPostimgData(Map<String, Object> map) {
-		System.out.println("map : acode(구분값:1:공지사항/2:이벤트/3:문의게시판/4:캠핑/5:관광지/6:커뮤니티 )" + map.get("acode") 
+		System.out.println("map acode(구분값:1:공지사항/2:이벤트/3:문의게시판/4:캠핑/5:관광지/6:커뮤니티 ) : " + map.get("acode") 
 						+ ",imgname(이미지명) :"+ map.get("imgname"));
 		int cnt = sqlSessionTemplate.insert(NAMESPACE + ".insertPostimgData", map);
+		return cnt;
+	}
+	
+	public int updatePostimgData(Map<String, Object> map) {
+		System.out.println("map : acode : " + map.get("acode") 
+						+ ",imgname(이미지명) :"+ map.get("imgname"));
+		int cnt = sqlSessionTemplate.update(NAMESPACE + ".updatePostimgData", map);
+		return cnt;
+	}
+	
+	public int deletePostimgData(Map<String, Object> map) {
+		System.out.println("map : acode : " + map.get("acode")
+							+ ", anum(각테이블 고유번호값): :"+ map.get("anum"));
+		
+		List<PostimgBean> lists = sqlSessionTemplate.selectList(NAMESPACE + ".getPostimgList", map);
+		
+		//System.out.println("lists:" + lists.toString());
+		
+		//내 해당테이블 고유번호에 묶여있는 이미지들 다 삭제하기.
+		int cnt =-1;
+		Map<String, Integer> imgMap = new HashMap<String, Integer>();
+		for(int i = 0; i < lists.size(); i++) {
+			imgMap.put("num", lists.get(i).getNum()); //이미지고유번호
+			cnt = sqlSessionTemplate.delete(NAMESPACE + ".deletePostimgData", imgMap);
+		}
+		
 		return cnt;
 	}
 	
