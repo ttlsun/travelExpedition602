f9bb92387d04f19ed8cebbfb905a6b85fc006034
KimBaek-Seyeong <saylovekim2186@gmail.com>
2021-10-12 12:05:56 +0900

    세영 - 일단 로그아웃 추가

---
+++ src/main/java/user/users/controller/UsersLogoutController.java
@@ -0,0 +1,30 @@
+package user.users.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import user.users.model.UsersDao;
+
+@Controller
+public class UsersLogoutController {
+	private final String COMMAND = "/logout.do";
+	private final String GOTOPAGE = "redirect:/main"; //메인 화면
+	
+	@Autowired
+	UsersDao usersDao;
+	
+	@RequestMapping(value=COMMAND)
+	public ModelAndView doAction(ModelAndView mav, HttpSession session) {
+		System.out.println(this.getClass()); //추후 삭제 가능
+		
+		//session.removeAttribute("loginInfo");
+		session.invalidate();
+		
+		mav.setViewName(GOTOPAGE);
+		return mav;
+	}
+}
